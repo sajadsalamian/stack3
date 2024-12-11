f01b2278b576e15b346e0751800ab25112622f8a
sajad <salamian.sajad@gmail.com>
2024-12-11 15:55:29 +0330

    7

--- src/components/Layouts/Main/Header.tsx
+++ src/components/Layouts/Main/Header.tsx
@@ -1,3 +1,4 @@
+import { Link } from "react-router-dom";
 
 export default function Header() {
   return (
@@ -5,10 +6,10 @@ export default function Header() {
       <header className="fixed top-0 left-0 right-0 flex p-4 gap-x-4 z-50 bg-dark w-full">
         <ul className="flex gap-x-4 text-white w-full items-center">
           <li>
-            <a href="/">Home</a>
+            <Link to="/">Home</Link>
           </li>
           <li>
-            <a href="/leaderboard">leaderboard</a>
+            <Link to="/leaderboard">leaderboard</Link>
           </li>
 
           <li className="ml-auto"></li>
