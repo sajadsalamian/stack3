a5a09522f1e379f26740b36ac8b46f8ee3ecea8a
sajad <salamian.sajad@gmail.com>
2024-12-21 21:17:22 +0330

    37

--- src/Pages/Splash.tsx
+++ src/Pages/Splash.tsx
@@ -32,7 +32,8 @@ export default function Splash() {
 
     axios({
       method: "post",
-      url: import.meta.env.VITE_API_URL + "/user",
+      url: "https://rust-api-gyot.shuttle.app/api/user",
+      // url: import.meta.env.VITE_API_URL + "/user",
       data: userData,
     })
       .then((res) => {
